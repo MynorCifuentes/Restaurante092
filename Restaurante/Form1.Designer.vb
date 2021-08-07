1b0052e14512ab94e09b380d3352673a59916adf
Mynor Cifuentes <mynoracifuentes09@gmail.com>
2021-08-06 23:08:49 -0600

    boton agregar listo

---
+++ Restaurante/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txt_cantidad = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cmb_tipo = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btn_agregar = New System.Windows.Forms.Button()
+        Me.txt_sub = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txt_cantidad)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.cmb_tipo)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Font = New System.Drawing.Font("Comic Sans MS", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.GroupBox1.Location = New System.Drawing.Point(38, 112)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(334, 229)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Menu"
+        '
+        'txt_cantidad
+        '
+        Me.txt_cantidad.Font = New System.Drawing.Font("Comic Sans MS", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txt_cantidad.Location = New System.Drawing.Point(143, 124)
+        Me.txt_cantidad.Name = "txt_cantidad"
+        Me.txt_cantidad.Size = New System.Drawing.Size(52, 40)
+        Me.txt_cantidad.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Comic Sans MS", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(3, 139)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(108, 31)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Cantidad"
+        '
+        'cmb_tipo
+        '
+        Me.cmb_tipo.Font = New System.Drawing.Font("Comic Sans MS", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.cmb_tipo.FormattingEnabled = True
+        Me.cmb_tipo.Items.AddRange(New Object() {"Menu 1", "Menu 2", "Menu 3", "Menu 4"})
+        Me.cmb_tipo.Location = New System.Drawing.Point(143, 58)
+        Me.cmb_tipo.Name = "cmb_tipo"
+        Me.cmb_tipo.Size = New System.Drawing.Size(151, 39)
+        Me.cmb_tipo.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Comic Sans MS", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(6, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 31)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Tipo"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(161, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(617, 84)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Restaurante Baby❤"
+        '
+        'btn_agregar
+        '
+        Me.btn_agregar.Font = New System.Drawing.Font("Comic Sans MS", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_agregar.Location = New System.Drawing.Point(105, 347)
+        Me.btn_agregar.Name = "btn_agregar"
+        Me.btn_agregar.Size = New System.Drawing.Size(175, 38)
+        Me.btn_agregar.TabIndex = 2
+        Me.btn_agregar.Text = "Agregar ❤"
+        Me.btn_agregar.UseVisualStyleBackColor = True
+        '
+        'txt_sub
+        '
+        Me.txt_sub.Font = New System.Drawing.Font("Comic Sans MS", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txt_sub.Location = New System.Drawing.Point(122, 412)
+        Me.txt_sub.Name = "txt_sub"
+        Me.txt_sub.Size = New System.Drawing.Size(125, 40)
+        Me.txt_sub.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(921, 504)
+        Me.Controls.Add(Me.txt_sub)
+        Me.Controls.Add(Me.btn_agregar)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txt_cantidad As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cmb_tipo As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btn_agregar As Button
+    Friend WithEvents txt_sub As TextBox
+End Class
---
+++ Restaurante/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub btn_agregar_Click(sender As Object, e As EventArgs) Handles btn_agregar.Click
+        If cmb_tipo.SelectedItem = "Menu 1" Then
+            txt_sub.Text = 2
+        End If
+    End Sub
+End Class
